3f68fdf2909d5fb9f0c955fb4666c707a22ec8f9
hbodan <hbodan@uamv.edu.ni>
2025-11-03 18:13:50 -0600

    Segundo ejercicio

--- src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import org.openxava.annotations.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity @Getter @Setter
 public class Producto {
 
@@ -12,5 +14,22 @@ public class Producto {
 
     @Column(length=50) @Required
     String descripcion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @DescriptionsList
+    Categoria categoria;
+
+    @Money
+    BigDecimal precio;
+
+    @Files
+    @Column(length = 32)
+    String fotos;
+
+    @TextArea
+    String observaciones;
+
+
+
 
 }
